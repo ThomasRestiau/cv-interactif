1b1bd740a3cff89bcd50f90eb5b33bcba52627da
Thomas Restiau <thomas.restiau@live.be>
2025-07-15 20:44:54 +0200

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ['react', '@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+
+    },
+};
